da1211ad3b8d0dc48ee6d61da1734681db056ae3
Mandana Soltani <mandanasoltanig@gmail.com>
2026-05-05 23:22:28 +0200

    update seeding

--- seeding.sql.sql
+++ seeding.sql.sql
@@ -1,2 +1,49 @@
 USE lab_mysql;
-SELECT * FROM cars;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE Invoices;
+TRUNCATE TABLE Cars;
+TRUNCATE TABLE Customers;
+TRUNCATE TABLE Salespersons;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+USE lab_mysql;
+
+INSERT INTO cars (VIN, manufacturer, model, year, color)
+VALUES
+('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
+('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
+('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray');
+
+INSERT INTO Customers 
+(`Customer ID`, name, `phone number`, email, address, city, `state/province`, country, `zip/postal code`)
+VALUES
+(10001, 'Pablo Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+(20001, 'Abraham Lincoln', '+1 305 907 7086', NULL, '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
+(30001, 'Napoléon Bonaparte', '+33 1 79 75 40 00', NULL, '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+
+INSERT INTO Salespersons (`staff ID`, name, store)
+VALUES
+(1, 'Petey Cruiser', 'Madrid'),
+(2, 'Anna Sthesia', 'Barcelona'),
+(3, 'Paul Molive', 'Berlin'),
+(4, 'Gail Forcewind', 'Paris'),
+(5, 'Paige Turner', 'Mimia'),
+(6, 'Bob Frapples', 'Mexico City'),
+(7, 'Walter Melon', 'Amsterdam'),
+(8, 'Shonda Leer', 'São Paulo');
+
+SELECT COUNT(*) FROM Cars;
+SELECT COUNT(*) FROM Customers;
+SELECT COUNT(*) FROM Salespersons;
+SELECT COUNT(*) FROM Invoices;
+INSERT INTO Invoices 
+(`invoice number`, date, VIN, customer_id, salesperson_id)
+VALUES
+(852399038, '2018-08-22', '3K096I98581DHSNUP', 1, 3),
+(731166526, '2018-12-31', 'RKXVNNIHLVVZOUB4M', 3, 5),
+(271135104, '2019-01-22', 'ZM8G7BEUQZ97IH46V', 2, 7);
